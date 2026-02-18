b10647b70eb450ea7736462136f1a51b626d8b0c
veni <krishnaveniar16@gmail.com>
2026-02-18 21:35:43 +0530

    infra: add backend app service

--- infra/main.tf
+++ infra/main.tf
@@ -11,3 +11,19 @@ resource "azurerm_service_plan" "app_plan" {
   os_type  = "Linux"
   sku_name = "B1"
 }
+resource "azurerm_linux_web_app" "backend" {
+  name                = "backend-devops-assignment"
+  location            = azurerm_resource_group.app.location
+  resource_group_name = azurerm_resource_group.app.name
+  service_plan_id     = azurerm_service_plan.app_plan.id
+
+  https_only = true
+
+  site_config {
+    always_on = true
+  }
+
+  app_settings = {
+    ENVIRONMENT = "prod"
+  }
+}
